cb7bc1600ebc725e3bc94e70f03adc65d99c86fb
Ubuntu <ubuntu@ip-172-31-16-148.ec2.internal>
2020-05-22 14:57:16 +0000

    json

--- tegaki.py
+++ tegaki.py
@@ -1,14 +1,11 @@
-import sys
-sys.path.append('/home/ubuntu/anaconda3/lib/python3.6/site-packages')
 from flask import Flask, jsonify
 from flask_cors import CORS
-
 app = Flask(__name__)
 CORS(app)
 
 @app.route('/')
 def hello_world():
-    return jsonify({"text":"Hello World!!"})
+    return "Hello World!!!!"
 
 if __name__ == '__main__':
     app.run()
